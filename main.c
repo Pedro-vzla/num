68fe1e6ed661202ef40cde50b9de83e231825938
Pedro Rondón <pedroprondonr@gmail.com>
2021-04-13 15:08:49 -0400

    numbers transpiler (num), initial commit
    [WARNING]
    Try to maintain a useful and organised git log, preserve the log format
    
     ___________________________________________________________________________
    /\                                                                          \
    \_|                                                                         |
      |    This is a tag: [...]                                                 |
      |                                                                         |
      |    This are the only tags: [TODO], [NOTE], [WARNING]                    |
      |                                                                         |
      |    Tags should be:                                                      |
      |          * No more than 80 characters width.                            |
      |          * Always [WARNING] first, [NOTE] second, [TODO] third.         |
      |                                                                         |
      |    tags can contain:                                                    |
      |          * Text, puntuation, and symbols.                               |
      |          * Dates and Timestamps.                                        |
      |          * Lists (style at your choice, but uniform).                   |
      |          * File paths.                                                  |
      |          * URLs.                                                        |
      |                                                                         |
      |    If not tags needed, just write desired text.                         |
      |                                                                         |
      |    _____________________________________________________________________|_
       \__/______________________________________________________________________/

---
+++ main.c
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+/*
++----------------------------------------------------------------------------+
+| This program is free software; you can redistribute it and/or modify it    |
+| under the terms of the GNU General Public License as published by the      |
+| Free Software Foundation; version 2.                                       |
+|                                                                            |
+| This program is distributed in the hope that it will be useful, but        |
+| WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY |
+| or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License    |
+| for more details.                                                          |
+|                                                                            |
+| You should have received a copy of the GNU General Public License          |
+| along with this program. If not, see <https://www.gnu.org/licenses/>.      |
++----------------------------------------------------------------------------+
+
+main.c - When assembly of Simple Mind Map starts
+Last modified: 2020-10-22 18:46:12
+
+Copyright (c) 2020 Pedro Rondón <pedroprondonr@gmail.com>
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	
+	return 0;
+}
